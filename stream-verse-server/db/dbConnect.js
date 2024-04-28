9d9ded2b11b215c91978045ca2db8c030e11c16d
azzizulhakim810 <azzizul.hakim202@gmail.com>
2024-04-29 03:05:53 +0600

    keep working 4

--- stream-verse-server/db/dbConnect.js
+++ stream-verse-server/db/dbConnect.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 exports.dbConnection = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`MongoDB connected on host: ${connection.connection.host}`);
+    console.log(
+      `MongoDB connected on current host: ${connection.connection.host}`
+    );
   } catch (error) {
     console.log(error);
   }
